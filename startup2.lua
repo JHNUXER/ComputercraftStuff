e9798622df53099ff87de60250d626d5a0100cfc
John Kayne <johnhoganuke2@gmail.com>
2017-03-22 14:57:52 -0400

    Update startup2.lua

--- startup2.lua
+++ startup2.lua
@@ -1,20 +1,4 @@
-local Menu = {}
-function Menu:new(sId)
-	o = {}
-	setmetatable(o,self)
-	self.id = sId
-	self.index = 1
-	self.options = {}
-	self.addOption = function(opt)
-		table.insert(self,opt)
-	end
-	return o
-end
-
-local menus = {
-	Menu:new("mainmenu"),
-	Menu:new("files")
-}
+local c = function() term.clear() term.setCursorPos(1,1) end
 
 local function fileBrowser()
 	local index = 1
@@ -24,36 +8,83 @@ local function fileBrowser()
 	local finalList = {}
 	
 	local function resolve(p)
-		return "/"..shell.dir.."/"..p
+		return "/"..shell.dir().."/"..p
+	end
+	local function isDir(p)
+		return fs.isDir(resolve(p))
 	end
 	local function indexOf(p)
-		for i = 1,#dfs do
+		for i = 1,#finalList do
 			if finalList[i] == p then return i end
 		end
 	end
 	local function textify(text)
 		local k = text
 		if isDir(text) then k = "[ "..text.." ]" else k = "- "..text.." -" end
-		if indexOf(p) == index then k = "> "..k else k = "  "..k end
+		if indexOf(text) == index then k = "> "..k else k = "  "..k end
+		return k
 	end
 	local function draw()
-		term.clear()
-		term.setCursorPos(1,1)
-		for i = 1,#dfs do
-			local text = textify(finalList[i])
-			print(text)
+		c()
+		for i = 1,#finalList do
+			print(textify(finalList[i]))
 		end
+	end
+	local function moveIndexUp()
+		if index > 1 then
+			index = index - 1
+		else
+			index = #finalList
+		end
+	end
+	local function moveIndexDown()
+		if index < #finalList then
+			index = index + 1
+		else
+			index = 1
+		end
+	end
+	local function exec()
+		if not isDir(finalList[index]) then shell.run("edit "..resolve(finalList[index])) else shell.setDir(resolve(finalList[index])) end
 	end
 	local keyFunctions = {
 		["up"] = moveIndexUp,
 		["down"] = moveIndexDown,
-		["left"] = function() shell.setDir(fs.getDir(shell.dir())) end
-		["right"] = function() shell.run("")
+		["left"] = function() shell.setDir(fs.getDir(shell.dir())) end,
+		["right"] = function() return nil end,
+		["enter"] = exec,
+		["tab"] = function() return nil end
 	}
-	local function waitForKeyPress()
-		local key = getKeyPress()
-		
+	local function grabFiles()
+		dfs = fs.list(shell.dir())
+		for i = 1,#dfs do
+			if isDir(dfs[i]) then table.insert(dirs,dfs[i]) else table.insert(files,dfs[i]) end
+		end
+		for i = 1,#dirs do
+			table.insert(finalList,dirs[i])
+		end
+		for i = 1,#files do
+			table.insert(finalList,files[i])
+		end
+	end
+	local function clearFiles()
+		dfs = {}
+		dirs = {}
+		files = {}
+		finalList = {}
+	end
+	local function getKeyPress()
+		local _,k,_ = os.pullEvent("key")
+		local key = keys.getName(k)
+		keyFunctions[key]()
 	end
 	while true do
+		clearFiles()
+		grabFiles()
+		draw()
+		getKeyPress()
+		sleep(0.1)
 	end
 end
+
+fileBrowser()
